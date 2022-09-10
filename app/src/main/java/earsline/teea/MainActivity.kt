962bc15ff472018717ecd75cacf4e88c3e89188a
Zhenya20062 <matveeveech1986@bk.ru>
2022-09-10 15:54:42 +0300

    add CircularProgressIndicator

--- app/src/main/java/earsline/teea/MainActivity.kt
+++ app/src/main/java/earsline/teea/MainActivity.kt
@@ -4,10 +4,14 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
 import com.euzhene.webviewdemo.getUrlFromDataStore
 import com.euzhene.webviewdemo.saveUrl
@@ -19,14 +23,15 @@ import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import earsline.teea.ui.theme.WinlineTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 
 class MainActivity : ComponentActivity() {
     private val remoteConfig: FirebaseRemoteConfig = Firebase.remoteConfig
 
-    private var urlReady by mutableStateOf(false)
     private var shouldLaunchPlug by mutableStateOf(false)
     private var shouldLaunchWebView by mutableStateOf(false)
+    private var shouldShowLoading by mutableStateOf(true)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,36 +43,39 @@ class MainActivity : ComponentActivity() {
                     minimumFetchIntervalInSeconds = 3600
                 }
                 remoteConfig.setConfigSettingsAsync(configSettings)
-                remoteConfig.fetchAndActivate().addOnSuccessListener {
-                    urlReady = true
+                remoteConfig.fetchAndActivate().await()
+
+                val urlFromConfig = remoteConfig["url"].asString()
+                if (
+                    urlFromConfig.isEmpty() ||
+                    Build.MANUFACTURER.lowercase().contains("google") ||
+                    !containsSim(this@MainActivity)
+                    || isEmulator()
+                ) {
+                    shouldLaunchPlug = true
+                } else {
+                    saveUrl(this@MainActivity, urlFromConfig)
+                    shouldLaunchWebView = true
                 }
+
             } else {
                 shouldLaunchWebView = true
             }
+            shouldShowLoading = false
         }
 
         setContent {
             WinlineTheme {
-                LaunchedEffect(key1 = urlReady) {
-                    val url = remoteConfig["url"].asString()
-                    if (
-                        url.isEmpty() ||
-                        Build.MANUFACTURER.lowercase().contains("google") ||
-                        !containsSim(this@MainActivity)
-                        || isEmulator()
-                    ) {
-                        shouldLaunchPlug = true
-                    } else {
-                        saveUrl(this@MainActivity, url)
-                        shouldLaunchWebView = true
-                    }
-                }
-
                 if (shouldLaunchPlug) {
                     QuizScreen()
                 }
                 if (shouldLaunchWebView) {
                     WinWebView()
+                }
+                if (shouldShowLoading) {
+                    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        CircularProgressIndicator()
+                    }
                 }
             }
         }
